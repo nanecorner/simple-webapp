02204968b176631f725814b68d601be8fea04f57
nanecorner <daniel2000rincon@gmail.com>
2024-09-02 16:59:15 -0600

    webhook

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World2!</h1>");
+        response.getWriter().println("<h1>Hello, World!</h1>");
     }
 }
